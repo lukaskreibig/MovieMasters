3a216d3cc61f41283bc93ebbf5a843e8e8e35ca7
Lukas Kreibig <78150333+lukaskreibig@users.noreply.github.com>
2022-01-14 21:15:17 +0100

    Add Apollo Client

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, QueryResult } from '@apollo/client';
+
+
+
+
 
 // Initialize Apollo Client
 const client = new ApolloClient({
@@ -8,13 +12,28 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+const TEST = gql`
+  query TestMovie {
+    movieDetail(id:40336) 
+    {
+    movie
+      {original_title}
+    }
+  }
+`
+const movieData:QueryResult<any> = useQuery(TEST)
+console.log(movieData)
 
 export default function App() {
   return (
+    <ApolloProvider client={client}>
+
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+
+  </ApolloProvider>
   );
 }
 
