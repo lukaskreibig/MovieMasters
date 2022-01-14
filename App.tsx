ebf2c2598e3b60e643da4282b08db712f7b7225e
Lukas Kreibig <78150333+lukaskreibig@users.noreply.github.com>
2022-01-14 20:21:50 +0100

    Create MovieMasters
    first commit - push to Github

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://ql-movie-api.herokuapp.com/graphql',
+  cache: new InMemoryCache()
+});
+
 
 export default function App() {
   return (
